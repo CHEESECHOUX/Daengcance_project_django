d95e4f68d93e4d35c7f38715bdd2e98c65e8f53e
cheesechoux <gsoosuu@gmail.com>
2022-09-02 14:29:05 +0900

    Create: apps models views

--- daengcance_django/settings.py
+++ daengcance_django/settings.py
@@ -30,7 +30,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'corsheaders'
+    'corsheaders',
+    'users',
+    'petsitters',
+    'reviews',
+    'bookings',
+    'core'
 ]
 
 MIDDLEWARE = [
